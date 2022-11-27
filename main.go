2414d4c25c06b50e76c9c46fd9552e7d2a63d002
Rahul-D78 <rahul.kumar@aveshasystems.com>
2022-11-27 19:00:27 +0530

    concurrency(level-1): using gorutines to spawn mutiple processes
    Signed-off-by: Rahul-D78 <rahul.kumar@aveshasystems.com>

--- main.go
+++ main.go
@@ -2,66 +2,27 @@ package main
 
 import (
 	"fmt"
-	"strings"
+	"time"
 )
 
-// var remoteRouteMap map[string][]string
+func attack(target string) {
+	fmt.Println("Throwing ninja stars at", target)
+	time.Sleep(1 * time.Second)
+}
 
 func main() {
+	// checking time taken to run the func
+	start := time.Now()
+	defer func() {
+		fmt.Println(time.Since(start))
+	}()
 
-	subsetAddrs := make([]string, 0)
-	fmt.Println(subsetAddrs)
-	a := "I am a"
-	b := "slice-rbac-deployment-role"
-
-	subsetAddrs = append(subsetAddrs, a)
-	fmt.Println(subsetAddrs)
-
-	rolebindingNameArr := strings.Split(b, "-")
-	var rolebindingName []string
-	fmt.Println(rolebindingNameArr)
-
-	for i, name := range rolebindingNameArr {
-		if i == len(rolebindingNameArr)-1 {
-			continue
-		}
-		rolebindingName = append(rolebindingName, name)
+	evilNinjas := []string{"Nano", "Pico", "Fermi", "Auto"}
+	for _, evilNinja := range evilNinjas {
+		// Using go keyword to spawn multiple goroutines / processes
+		go attack(evilNinja)
 	}
-	newRolebindingName := strings.Join(rolebindingName, "-")
-	fmt.Println(newRolebindingName + "-rolebinding")
 
-	// a := "I am a"
-	// b := "I am b"
-	// c := "I am c"
-
-	// var strarr = []string{"abcd", "efgh"}
-
-	// remoteRouteMap = make(map[string][]string)
-
-	// for i := 0; i < len(strarr)-1; i++ {
-	// 	fmt.Println(strarr[0])
-	// 	remoteRouteMap[a] = append(remoteRouteMap[a], strarr[0])
-	// }
-
-	// remoteRouteMap[a] = append(remoteRouteMap[a], b)
-	// remoteRouteMap[a] = append(remoteRouteMap[a], c)
-
-	// var count = 0
-
-	// for i := 0; i < len(remoteRouteMap[a]); i++ {
-	// 	count++
-	// 	// fmt.Println(remoteRouteMap[a][0])
-
-	// 	if reflect.DeepEqual(remoteRouteMap[a][i], c) {
-	// 		fmt.Println("Matched")
-	// 	}
-
-	// }
-
-	// fmt.Println(remoteRouteMap)
-
-	// for _, val := range remoteRouteMap {
-	// 	fmt.Println(len(val))
-	// }
-
+	// For temp purpose we can wait for 2 sec to exit out of main process
+	time.Sleep(2 * time.Second)
 }
