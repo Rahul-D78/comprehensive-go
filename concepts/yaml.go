89f83cd5ab63dc91786024df015812651a47bfd3
Rahul-D78 <rahul.kumar@aveshasystems.com>
2024-07-17 17:48:09 +0530

    fix(): yaml file management using yaml pkg
    Signed-off-by: Rahul-D78 <rahul.kumar@aveshasystems.com>

--- concepts/yaml.go
+++ concepts/yaml.go
@@ -1,46 +1,65 @@
 package concepts
 
 import (
+	"bufio"
 	"fmt"
+	"log"
 	"os"
+	"strings"
 
 	"gopkg.in/yaml.v2"
 )
 
-// YAML tags are used to specify how Go struct fields should be encoded as YAML data.
-type Person struct {
-	Name string `yaml:"name,omitempty"`
-	Age  int    `yaml:"age"`
+type GitProperties struct {
+	CommitPrefix  string `json:"commitPrefix,omitempty"`
+	PrDescription string `json:"prDescriptionFormat,omitempty"`
 }
 
 func YamlFunc() error {
-	person := &Person{
-		Name: "hello",
-		Age:  10,
+	gp := &GitProperties{
+		CommitPrefix:  "abc",
+		PrDescription: "efg",
 	}
 
-	y, err := yaml.Marshal(person)
+	g, err := yaml.Marshal(gp)
 	if err != nil {
 		return err
 	}
-	fmt.Println("Yaml marshled data", y)
+	fmt.Println("Yaml marshled data for git properties", string(g))
 
-	err = os.WriteFile("config.yaml", y, 0644)
-	if err != nil {
-		return err
-	}
-
-	source, err := os.ReadFile("config.yaml")
+	err = os.WriteFile("gp.yaml", g, 0644)
 	if err != nil {
 		return err
 	}
 
-	p := &Person{}
-	err = yaml.Unmarshal(source, p)
+	source, err := os.Open("gp.yaml")
 	if err != nil {
 		return err
 	}
-	fmt.Println(p.Name)
+
+	var lines []string
+
+	scanner := bufio.NewScanner(source)
+	scanner.Split(bufio.ScanLines)
+
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+	if err := scanner.Err(); err != nil {
+		log.Fatalf("Error reading file: %v", err)
+	}
+	fmt.Println(lines)
+
+	// Parse YAML data into struct
+	var gitp GitProperties
+	yamlData := strings.Join(lines, "\n")
+	err = yaml.Unmarshal([]byte(yamlData), &gitp)
+	if err != nil {
+		log.Fatalf("Error parsing YAML: %v", err)
+	}
+
+	fmt.Println()
+	fmt.Println("Server:", gitp.CommitPrefix)
 
 	return nil
 }
