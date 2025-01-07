bf18a2f64f521204b03c900389ca578804aeebbd
Rahul-D78 <rahul.kumar@aveshasystems.com>
2025-01-07 20:04:55 +0530

    fix(): add todo commits
    Signed-off-by: Rahul-D78 <rahul.kumar@aveshasystems.com>

---
+++ server/server.go
@@ -0,0 +1,39 @@
+package server
+
+type server struct {
+}
+
+// grpc server connection
+// TODO: add a MakeFile also
+// also include how can we create a bridge, switch and a router using netlink
+// then how can we create bgp connections
+// also client-go programming
+// as well as netfilter queue for packet filtering - fierwall software
+// as well as nftables create nat tables using go programs
+// then we can also write some code about cuda programming and nvml, nvlink for gpu programmnig
+
+// also please include some code for decorated type alias
+
+// also how can we create a plugin based system for golang
+
+// mainly we can use this repo to showcase my golang programming knowledge
+// how to better organize the codebase into the folders - we can also give a link to the
+// openkubestack project where we have a lot of operators and lot of integration arround tools
+
+// also maintain a good readme - talk about oops concepts, golang design patters
+//
+// golang compiler optimization techniques
+//
+// how to use pprof for profiling - https://www.youtube.com/watch?v=0_oTkC6HYPA&t=102s
+
+// how to write test cases for the application, use of interfaces to write mocks - we can call it emulator
+
+// ALSO ONE REQUEST TO GO THROUGH THE HIGH PERFORMANCE PROGRAMMING GOLANG SERIES ON medium.
+//
+// and see if we can understand the golang concurrency patters - https://www.youtube.com/watch?v=f6kdp27TYZs&pp=ygUbZ29sYW5nIGNvbmN1cnJlbmN5IHBhdHRlcm5z
+//
+// then this one too - https://www.youtube.com/watch?v=QDDwwePbDtw&pp=ygUbZ29sYW5nIGNvbmN1cnJlbmN5IHBhdHRlcm5z
+
+func NewServer() *server {
+	return &server{}
+}
